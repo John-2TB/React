7dcca0d2054689d1588aa54cf2a4196cdbb8b4bf
John-2TB <johnkoto2tb@gmail.com>
2026-08-25 18:59:37 +0100

    Added Tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
+  plugins: [react(), tailwindcss()],
 })
