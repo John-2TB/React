2ec420f57337af8a4c5ec2ad0e5d033798a875df
John-2TB <johnkoto2tb@gmail.com>
2026-08-26 10:07:05 +0100

    Header design implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-image.png" alt="" />
+          <h1>Find <span className='text-gradient'>Animes</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h1 className='text-white text-2xl'>{searchTerm}</h1>
+        </header>
+
+        
+      </div>
+    </main>
   )
 }
 
